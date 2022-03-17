d0dbc3fd296e559187ecf485aa33afd7c3ee9bb9
Nisarg851 <nisargmahyavanshi8501@gmail.com>
2022-03-17 18:26:08 +0530

    Databinding added

--- app/src/main/java/com/example/quoteandjet/MainActivity.kt
+++ app/src/main/java/com/example/quoteandjet/MainActivity.kt
@@ -6,48 +6,54 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.quoteandjet.Models.Quote
 import com.example.quoteandjet.ViewModelFactories.MainViewModelFactory
 import com.example.quoteandjet.ViewModels.MainViewModel
+import com.example.quoteandjet.databinding.ActivityMainBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
 
     // View Components
-        private lateinit var tvQuote: TextView
-        private lateinit var tvAuthor: TextView
+//        private lateinit var tvQuote: TextView
+//        private lateinit var tvAuthor: TextView
+//
+//        private lateinit var btnShare: FloatingActionButton
+//        private lateinit var btnNext: Button
+//        private lateinit var btnPrevious: Button
 
-        private lateinit var btnShare: FloatingActionButton
-        private lateinit var btnNext: Button
-        private lateinit var btnPrevious: Button
-
-    private var TAG = "Lifecycler"
+//    private var TAG = "Lifecycler"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 //        lifecycle.addObserver(LifeCycleObserverClass())
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this, MainViewModelFactory(application)).get(MainViewModel::class.java)
 
         setControls()
     }
 
     private fun setControls() {
-        tvQuote = findViewById(R.id.tvQuote)
-        tvAuthor = findViewById(R.id.tvAuthor)
-        btnShare = findViewById(R.id.fbtnShare)
-        btnPrevious = findViewById(R.id.btnPrevious)
-        btnNext = findViewById(R.id.btnNext)
+        binding.tvQuote.text = viewModel.getQuote().text
+        binding.tvAuthor.text = viewModel.getQuote().author
+//        tvQuote = findViewById(R.id.tvQuote)
+//        tvAuthor = findViewById(R.id.tvAuthor)
+//        btnShare = findViewById(R.id.fbtnShare)
+//        btnPrevious = findViewById(R.id.btnPrevious)
+//        btnNext = findViewById(R.id.btnNext)
+//
+//        tvQuote.text = viewModel.getQuote().text
+//        tvAuthor.text = viewModel.getQuote().author
 
-        tvQuote.text = viewModel.getQuote().text
-        tvAuthor.text = viewModel.getQuote().author
-
-        btnShare.setOnClickListener(this)
-        btnNext.setOnClickListener(this)
-        btnPrevious.setOnClickListener(this)
+        binding.fbtnShare.setOnClickListener(this)
+        binding.btnNext.setOnClickListener(this)
+        binding.btnPrevious.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
@@ -63,15 +69,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
             R.id.btnNext -> {
                 val quote: Quote = viewModel.getNextQuote()
-                tvQuote.text = quote.text
-                tvAuthor.text = quote.author
+                binding.tvQuote.text = quote.text
+                binding.tvAuthor.text = quote.author
             }
 
             R.id.btnPrevious -> {
                 val quote: Quote = viewModel.getPreviousQuote()
-                tvQuote.text = quote.text
-                tvAuthor.text = quote.author
+                binding.tvQuote.text = quote.text
+                binding.tvAuthor.text = quote.author
             }
+
         }
     }
 
